07ceb61c612af598248d1266ff20ea0cd808c771
unknown <Zeelgabriels>
2024-09-18 00:52:44 +0100

    CSS classes and navigation links on footer components for the application

---
+++ src/components/Footer.js
@@ -0,0 +1,76 @@
+import { Link } from "react-router-dom";
+export default function Footer() {
+  return (
+    <footer>
+      <menu>
+        <li className="img-footer">
+          <img
+            src={require("../assets/footer-logo.png")}
+            alt="Treeats logo"
+          ></img>
+        </li>
+        <li className="contact">
+          <h1 className="footer-header">Navigation</h1>
+          <ul className="footer-links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <a
+              href={require("../assets/menu.webp")}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Menu
+            </a>
+            <Link to="/reservations">Reservations</Link>
+            <Link to="/order">Order</Link>
+            <Link to="/login">Login</Link>
+          </ul>
+        </li>
+
+        <li>
+          <h1 className="footer-header">Contact</h1>
+          <ul className="footer-links">
+            <li>15 Nzimiro Street,</li>
+            <li>Old GRA, Port Harcourt, Rivers</li>
+            <br></br>
+            <li>+2348132887088</li>
+            <br></br>
+            <a
+              href="mailto: info@treetscuisine.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              info@treetscuisine.com
+            </a>
+          </ul>
+        </li>
+        <li>
+          <h1 className="footer-header">Connect</h1>
+          <ul className="footer-links">
+            <a
+              href="https://www.facebook.com/treeatscuisine/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Facebook
+            </a>
+            <a
+              href="https://www.instagram.com/treeatscuisine/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Instagram
+            </a>
+            <a
+              href="https://www.youtube.com/treeatscuisine/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Join us!
+            </a>
+          </ul>
+        </li>
+      </menu>
+    </footer>
+  );
+}
