5680259e640bff2f554b95a0f98c0c5c3656ef95
unknown <Zeelgabriels>
2024-09-19 02:48:05 +0100

    Header section components for About us page on the application

--- src/components/sections/orderPages/Heading.js
+++ src/components/sections/orderPages/Heading.js
@@ -1,11 +1,11 @@
-import HeroImage from "../../../assets/food/food4.webp";
+import HeroImage from "../../../assets/food/food4.wepb";
 export default function Heading() {
   return (
     <header className="reserve-table">
       <img
         className="header-reserve"
         src={HeroImage}
-        alt="Little Lemon Ingredients"
+        alt="Treeats Ingredients"
       ></img>
       <div className="reserve-header-text">
         <h1>Order Online</h1>
